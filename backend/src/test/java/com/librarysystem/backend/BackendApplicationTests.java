dfa2b4731ccfc422f2ff466727b6055a91dff3a5
Nortey Daniel Yeboah <gogamerdan247@gmail.com>
2026-06-29 11:21:54 +0000

    project initialization

---
+++ backend/src/test/java/com/librarysystem/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarysystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
